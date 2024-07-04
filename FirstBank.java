24e7b9f786f0c62bb71cf1c43578f3a79ad397e9
suresh-2201 <sureshkumar0302@gmail.com>
2024-07-04 09:24:04 +0530

    Add initial changes

---
+++ FirstBank.java
@@ -0,0 +1,9 @@
+public class FirstBank {
+    public static void main(String[] args) {
+        Account s = new Account("Suresh kumar", "A0001");
+        s.showMenu();
+
+        Account beyonce = new Account("Dhiraj kumar", "B0001");
+        beyonce.showMenu();
+    }
+}
